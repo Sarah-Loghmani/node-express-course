db3e0bd7f884b9acf6eefa6ae9d6bd153c81ed1b
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-15 12:14:49 +0430

    1

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+
+//routes
+app.get('/', (req, res)=>{
+    res.send("Task Manger APP");
+})
+
+//app.get('/api/v1/tasks')       => get all the tasks
+//app.post('/api/v1/tasks')      => create a new task
+//app.get('/api/v1/tasks/:id')   => get single task
+//app.patch('/api/v1/tasks/:id') => update task
+//app.delete('/api/v1/tasks/:id')=> delete task
+
+
+const PORT= 3000;
+
+app.listen(PORT, console.log(`Server is listen on port ${PORT}`));
