e6c3a88e61e742c19a10fbb57709cc47e7164c0e
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-17 17:37:28 +0430

    1

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
+//Middleware for accessing the data
+app.use(express.json());
 
-//routes
-app.get('/', (req, res)=>{
-    res.send("Task Manger APP");
-})
+//*routes
+// app.get("/hello", (req, res) => {
+//   res.send("Task Manger APP");
+// });
+
+app.use("/api/v1/tasks", tasks);
 
 //app.get('/api/v1/tasks')       => get all the tasks
 //app.post('/api/v1/tasks')      => create a new task
@@ -13,7 +18,6 @@ app.get('/', (req, res)=>{
 //app.patch('/api/v1/tasks/:id') => update task
 //app.delete('/api/v1/tasks/:id')=> delete task
 
-
-const PORT= 3000;
+const PORT = 3000;
 
 app.listen(PORT, console.log(`Server is listen on port ${PORT}`));
