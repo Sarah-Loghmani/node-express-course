8202749e19ca1a1767b97ec2cde36a1ba4c01601
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-17 17:37:41 +0430

    23

---
+++ 03-task-manager/starter/routes/tasks.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/tasks");
+
+// console.log(express);
+
+// router.route("/").get((req, res) => {
+//   res.send("all items");
+// });
+// instead of this line to prevent get messy in this file we go to the controller folder and create task.js file and write it there. and change the code:
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+
+module.exports = router;
