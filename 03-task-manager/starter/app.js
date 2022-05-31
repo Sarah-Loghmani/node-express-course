fd86606c94f58819dfbaf440bbdfab7d5d9211c6
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-31 23:44:48 +0430

    3

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/tasks", tasks);
 //app.patch('/api/v1/tasks/:id') => update task
 //app.delete('/api/v1/tasks/:id')=> delete task
 
-const PORT = 3000;
+const PORT = 8000;
 
 const start = async () => {
   try {
