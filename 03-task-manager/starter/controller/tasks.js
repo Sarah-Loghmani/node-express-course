d8f8efde93806f26db97bc42945e2ef2fec658db
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-31 23:44:56 +0430

    4

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,17 +1,65 @@
-const getAllTasks = (req, res) => {
-  res.send("Get all tasks");
+const Task = require("../models/Task");
+
+// createTask
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ ERRORmsg: error });
+  }
+};
+//getAllTasks
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ ERRORmsg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+//getTask
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ ERRORmsg: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update task");
+
+//deleteTask
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOnAndDelete({ _id: taskID });
+    !task
+    ? res.status(404).json({ msg: `No task with id: ${taskID}` })
+    : res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ ERRORmsg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete tasks");
+//updateTask
+const updateTask = async(req, res) => {
+  try {
+    const {id: taskID}= req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID}, req.body)
+
+    !task
+      ? res.status(404).json({ msg: `No task with id: ${taskID}` })
+      : res.status(200).json({ task });
+
+  } catch (error) {
+    res.status(500).json({ ERRORmsg: error });
+  }
 };
 
 module.exports = {
