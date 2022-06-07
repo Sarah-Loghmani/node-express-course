2602d6fc7f827a53bee7bf24c02f6b8c33fe0573
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-07 23:21:34 +0430

    8

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,10 +4,14 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //Middleware for accessing the data
+app.use(express.static("./public"));
 app.use(express.json());
 
 //*routes
@@ -16,6 +20,8 @@ app.use(express.json());
 // });
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound)
+app.use(errorHandlerMiddleware);
 
 //app.get('/api/v1/tasks')       => get all the tasks
 //app.post('/api/v1/tasks')      => create a new task
